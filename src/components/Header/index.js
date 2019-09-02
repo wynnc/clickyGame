75aaa9b173d397e8e8f5a87377b7252183898aeb
Wynn Chaney <wynn.chaney@wanco.com>
2019-09-02 12:02:19 -0600

    Created file structure, and header file.

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style";
+
+function Header() {
+    render (
+        <header className="header">
+            <h1>Clicky Game!</h1>
+            <h2>Click on image to earn points, but don't click on image more than once!</h2>
+        </header>
+    )
+}
