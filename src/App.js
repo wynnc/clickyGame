4832d9b3de6174c8249ff87f6f3464dcf2d54bec
Wynn Chaney <wynn.chaney@wanco.com>
2019-09-02 18:40:37 -0600

    created header.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+
 
 function App() {
   return (
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./style";
+import "./style.css";
 
 function Header() {
-    render (
+    return(
         <header className="header">
             <h1>Clicky Game!</h1>
             <h2>Click on image to earn points, but don't click on image more than once!</h2>
         </header>
     )
 }
+
+export default Header;
