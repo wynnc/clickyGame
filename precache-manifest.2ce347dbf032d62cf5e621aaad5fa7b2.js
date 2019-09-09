3bf73ec6047125b7df90f801b1e7288078f7fcd8
Wynn Chaney <wynn.chaney@wanco.com>
2019-09-09 12:38:16 -0600

    Updates

---
+++ precache-manifest.2ce347dbf032d62cf5e621aaad5fa7b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbbbcd331e37f7c12369c8701605a39",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e14b29800d324bb14833",
+    "url": "/clickyGame/static/css/main.5cddafae.chunk.css"
+  },
+  {
+    "revision": "34deb1cbacf60f576cbe",
+    "url": "/clickyGame/static/js/2.7041434a.chunk.js"
+  },
+  {
+    "revision": "e14b29800d324bb14833",
+    "url": "/clickyGame/static/js/main.59a6bc26.chunk.js"
+  },
+  {
+    "revision": "b04e682f02a4fb6b28c9",
+    "url": "/clickyGame/static/js/runtime~main.f5135437.js"
+  }
+]);
