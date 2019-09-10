115961796ec60f70308d8ac2ba85132c64114803
Wynn Chaney <wynn.chaney@wanco.com>
2019-09-10 08:42:52 -0600

    Updated header message

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
     return(
         <header className="header">
             <h1>Clicky Game!</h1>
-            <h2>Click on image to earn points, but don't click on image more than once!</h2>
+            <h2>Click on the images to earn points, but don't click on an image more than once!</h2>
         </header>
     )
 }
